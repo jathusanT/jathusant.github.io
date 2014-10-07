f947db21a8c1c400b5eb6fb2881f6de6a9fcff2c
Jathusan Thiruchelvanathan <jathusan.t@gmail.com>
2014-10-07 00:42:29 -0700

    added parallax image dividers between sections

--- js/index.js
+++ js/index.js
@@ -2,3 +2,11 @@ $('.scrollLink').on("click", function(event){
 	event.preventDefault();
 	$.scrollTo($(event.target.hash), 500);
 });
+
+$('.img-holder').imageScroll({
+	coverRatio: 0.35
+});
+
+$('.img-holder2').imageScroll({
+	coverRatio: 0.20
+});
